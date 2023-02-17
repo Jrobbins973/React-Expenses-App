b016d0dec6ff18e2c0972a12fff9565ad0d65c36
Joe Robbins <joe.robbins.cpt@gmail.com>
2023-02-17 17:13:22 -0500

    initial commmit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
+import Card from '../UI/Card'
+
+function ExpenseItem(props) {
+
+    const sayHello = () => {
+        console.log("Helo world")
+    }
+
+
+
+    return (
+        <Card className = 'expense-item'>
+            <div>
+
+                <ExpenseDate date={props.date}/>
+            </div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+            <button onClick={sayHello}>Change Title</button>
+        </Card>
+    )
+}
+
+export default ExpenseItem
